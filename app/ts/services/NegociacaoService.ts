4cff955ae0dbc803e3264deeb3311a6a7dd4c6c6
Vitor <vih.frez@gmail.com>
2018-07-20 15:37:41 -0300

    problema com Promise

--- app/ts/services/NegociacaoService.ts
+++ app/ts/services/NegociacaoService.ts
@@ -2,7 +2,7 @@ import { NegociacaoParcial, Negociacao } from '../models/index';
 
 export class NegociacaoService {
 
-    obterNegociacoes(handler: ResponseHandler): Promise<Negociacao[]> {
+    obterNegociacoes(handler: ResponseHandler): Promise<void| Negociacao[]> {
 
         return fetch('http://localhost:8080/dados')
             .then(res => handler(res))
@@ -12,7 +12,7 @@ export class NegociacaoService {
             )
             .catch(err => console.log(err));
 
-            
+
     }
 }
 
